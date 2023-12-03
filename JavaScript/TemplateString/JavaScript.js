f626713ff384aa1117c9ee35a9b5c33695364c53
Miray Durgun <miraydurgun67@gmail.com>
2023-12-03 19:34:09 +0300

    Template String kullanımı

---
+++ JavaScript/TemplateString/JavaScript.js
@@ -0,0 +1,9 @@
+﻿const framework1 = 'Angular';
+const framework2 = 'React';
+const framework3 = 'Vue';
+
+let sonuc = `Yazdırmak istediklerim ${framework1} ${framework2} ${framework3}`
+
+ley html = `
+<h1>${framework1}</h1>
+`
