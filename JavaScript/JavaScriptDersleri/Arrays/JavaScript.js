4b0b86226bfb33fefb2fd6aa87b25c4c791de1e0
Miray Durgun <miraydurgun67@gmail.com>
2023-12-03 19:51:39 +0300

    Arrays

---
+++ JavaScript/JavaScriptDersleri/Arrays/JavaScript.js
@@ -0,0 +1,24 @@
+﻿let isimler = ['Miray', 'Umut', 'Can', 'Aslı'];
+
+//console.log(isimler[1]);
+
+//let'i modify edebiliriz fakar consu edemeyiz
+
+isimler[1] = 'rabia';
+
+
+let yaslar = [11, 15, 50, 18];
+//console.log(yaslar[2]);
+
+let tireli = isimler.join('-');
+console.log(tireli);
+
+
+let bul = isimler.indexOf('Miray');
+console.log(bul);
+
+let ekle = isimler.concat(['Ekrem', 'Büşra', 'Özlem']);
+console.log(ekle);
+
+let elemanEkle = isimler.push('leyla');
+console.log(elemanEkle);
