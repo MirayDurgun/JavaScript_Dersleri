35da648fa0fa73c0501345dc08841fd6933f26c2
Miray Durgun <miraydurgun67@gmail.com>
2023-12-04 15:10:53 +0300

    for döngüsü

---
+++ JavaScript/JavaScriptDersleri/For_Dongusu/JavaScript.js
@@ -0,0 +1,11 @@
+﻿for (let i = 0; i < 10; i++) {
+    console.log(i);
+}
+console.log("Döngü bitti...");
+
+//*********************************************************************
+const kisi = ['ege', 'can', 'ece']
+
+for (let a = 0; a < kisi.length; a++)
+    console.log(kisi[a]);
+
