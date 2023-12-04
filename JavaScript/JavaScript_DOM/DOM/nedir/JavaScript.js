c18a7014fc7cc6384c8fa3009e9d91c5feb07a59
Miray Durgun <miraydurgun67@gmail.com>
2023-12-04 17:55:51 +0300

    DOM giriş

---
+++ JavaScript/JavaScript_DOM/DOM/nedir/JavaScript.js
@@ -0,0 +1,18 @@
+﻿const hata1 = document.querySelector('p'); //htmldeki ilk p tagini seçer, hepsini seçmez
+console.log(hata1);
+
+/**************************************************/
+
+const hata2 = document.querySelector('.error'); //htmldeki .error ilk error clasını manüpüle eder
+console.log(hata2);
+
+/**************************************************/
+
+const hata3 = document.querySelector('div.error'); //htmldeki divin içindeki .error olan error clasını manüpüle eder
+console.log(hata3);
+
+
+//P taglerinin hepsini seçme querySelectorAll
+
+const hatalar = document.querySelectorAll('p'); //htmldeki tüm p taglerini seçer
+console.log(hatalar);
