9cf7c3ba5670fbfafc16bac7f8baea816fa89a90
Miray Durgun <miraydurgun67@gmail.com>
2023-12-04 16:33:50 +0300

    Function tanımlama

---
+++ JavaScript/JavaScriptDersleri/Fonksiyon_Tanimlama/JavaScript.js
@@ -0,0 +1,30 @@
+﻿function miray() {
+    console.log('Miray');
+}
+miray();
+
+
+/*******************************************************/
+
+//değişken ismini fonksiyona atadık
+const mirayDurgun = function () {
+    console.log('deneme');
+}
+mirayDurgun();
+
+
+//Fonksiyona parametre verme
+const mirayDurgun2 = function (js, css, ang) {
+    console.log(js, css, ang);
+}
+mirayDurgun2('JS', 'Css', 'Angular');
+
+
+//Return
+const kareAlani = function (kenar) {
+    let alan = kenar ** 2  //**2 karesini alır
+    return alan;
+}
+const sonuc = kareAlani(6);
+console.log(sonuc);
+
