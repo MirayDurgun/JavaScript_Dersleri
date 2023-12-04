fe6084b63e3f340c33ef209d640f668c91d7d4b9
Miray Durgun <miraydurgun67@gmail.com>
2023-12-04 23:34:09 +0300

    get ve set attribute kullanımı

---
+++ JavaScript/JavaScript_DOM/DOM/Get_Set_Kullanim/JavaScript.js
@@ -0,0 +1,15 @@
+﻿const link = document.querySelector('a');
+console.log(link.getAttribute('href'));
+//hrefteki link console geldi
+
+//set ile yazıyı güncelleyeceğiz
+link.setAttribute('href', 'https://www.linkedin.com/in/miraydurgun/');
+// linki güncelledik
+
+//texti güncelleyeceğiz
+link.textContent = 'Linkedin linkim';
+
+//class ismi değiştirme
+const pDegeri = document.querySelector('p');
+console.log(pDegeri.getAttribute('class'));
+pDegeri.setAttribute('class', 'link');
