adccdb8b4fc47f29465224f0b51067afa8a9bd14
Miray Durgun <miraydurgun67@gmail.com>
2023-12-04 17:07:18 +0300

    Function&Method

---
+++ JavaScript/JavaScriptDersleri/Function&Method/JavaScript.js
@@ -0,0 +1,13 @@
+﻿//Function & Method arasındaki fark
+
+const ad = "miray";
+
+//arrow function
+const miray = () => 'Miray'
+let sonuc1 = miray();
+console.log(sonuc1);
+
+//method
+
+let sonuc2 = ad.toLocaleUpperCase();
+console.log(sonuc2);
