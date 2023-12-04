d9e6e12352abf07b97f99ed234589847dce85039
Miray Durgun <miraydurgun67@gmail.com>
2023-12-04 17:34:37 +0300

    Obje Tanımlama

---
+++ JavaScript/JavaScriptDersleri/Obje_Tanimlama/JavaScript.js
@@ -0,0 +1,34 @@
+﻿let ogrenci = {
+    ad: 'Miray',
+    yas: 21,
+    egitim: 'Lisans'
+};
+console.log(ogrenci);
+
+//objeye method ekleme
+let ogrenci2 = {
+    ad: 'Miray',
+    yas: 21,
+    egitim: 'Lisans',
+    login: function () {
+        console.log('Öğrenci giriş yaptı');
+    },
+    logout: function () {
+        console.log('Öğrenci çıkış yaptı');
+    },
+};
+ogrenci2.login();
+console.log(ogrenci2);
+ogrenci2.logout();
+
+//math objesi
+console.log(Math.PI);
+console.log(Math);
+
+let sayi = 5.7;
+console.log(Math.round(sayi)); //round sayıyı .dan sonra hangi sayıya yakınsa ona yuvarlar
+
+//floor 1 alta yuvarlar
+//ceil 1 üste yuvarlar
+//trunc decimal kısmınını uçurur
+//random, random sayı üretir
