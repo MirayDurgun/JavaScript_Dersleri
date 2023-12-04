237eb419cc71a003daf1686f8594b58363eaa83f
Miray Durgun <miraydurgun67@gmail.com>
2023-12-04 23:24:24 +0300

    sayfa içeriği değiştirme

---
+++ JavaScript/JavaScript_DOM/DOM/sayfa_icerigi_degistirme/JavaScript.js
@@ -0,0 +1,16 @@
+﻿
+//p tagini güncelleme
+const pTag = document.querySelector('p');
+console.log(pTag.innerText); //normalde html kodları ile beraber console ekranında veriliyordu, innerText yazınca sadece text değerini getirdi
+
+pTag.innerText = 'Miray Durgun'; //html kodunu manüpüle ettik
+
+
+
+//p taglerinin hepsinin yanına text ekleme
+const pTag2 = document.querySelectorAll('p');
+pTag2.forEach(a => {
+    console.log(a.innerText);
+    a.innerText += ' yeni alan'; //+= sayesinde olanı korur ve yanına yeni texti ekler
+})
+
