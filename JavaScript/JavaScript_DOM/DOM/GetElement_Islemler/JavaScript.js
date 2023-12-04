853321c3b1ac5b02f82115b9f893e32d33f7c8a5
Miray Durgun <miraydurgun67@gmail.com>
2023-12-04 22:54:45 +0300

    GetElementByID, GetElementsByClassName, GetElementsByTagName kullanımı

---
+++ JavaScript/JavaScript_DOM/DOM/GetElement_Islemler/JavaScript.js
@@ -0,0 +1,15 @@
+﻿//getElementById
+const baslik = document.getElementById('deneme');
+console.log(baslik);
+
+//getElementsByClassName
+//adından da belli olduğu gibi classı çekeceğimiz için çağırırken . kullanmamıza gerek yok
+const hatalar = document.getElementsByClassName('error');
+console.log(hatalar);
+console.log(hatalar[0]); //ilkini de çağırabiliriz
+
+//getElementsByTagName
+//html tagini çağırır / çeker
+
+const pEtiketleri = document.getElementsByTagName('p');
+console.log(pEtiketleri);
