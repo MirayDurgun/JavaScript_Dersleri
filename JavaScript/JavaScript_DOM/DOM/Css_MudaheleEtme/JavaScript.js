7a096d9650afa3d9b9cef8be50eeda34b82a0fdd
Miray Durgun <miraydurgun67@gmail.com>
2023-12-06 20:10:11 +0300

    Css e Müdahale Etme

---
+++ JavaScript/JavaScript_DOM/DOM/Css_MudaheleEtme/JavaScript.js
@@ -0,0 +1,8 @@
+﻿const baslik = document.querySelector('h1');
+baslik.setAttribute('style', 'color:pink;');
+//css olarak htmlde farklı style olsaydı bu js kodu css i ezerdi
+
+
+//bu sebeple bu şekilde kullanılmalı;
+const olmali = document.querySelector('h2');
+olmali.style.color = 'pink';
