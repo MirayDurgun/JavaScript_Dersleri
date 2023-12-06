a398cc956709d3fe7a2ac102fdd26b03acdec206
Miray Durgun <miraydurgun67@gmail.com>
2023-12-06 20:17:44 +0300

    Css Class ı Ekleme Kaldırma

---
+++ JavaScript/JavaScript_DOM/DOM/Css_Class_EkleveKaldir/JavaScript.js
@@ -0,0 +1,9 @@
+﻿//class ismi bastırma
+
+const icerik = document.querySelector('p');
+console.log(icerik.classList);
+//class ismi ekleme
+icerik.classList.add('miray');
+
+//class ismi kaldırma
+icerik.classList.remove('error');
