6d364c17beba0656de073dc2a25767e66dafebd1
Miray Durgun <miraydurgun67@gmail.com>
2023-12-06 22:04:13 +0300

    Popup

---
+++ JavaScript/JavaScript_DOM/DOM/Popup/JavaScript.js
@@ -0,0 +1,28 @@
+﻿const buton = document.querySelector('button');
+const maninPopup = document.querySelector('.main-popup');
+const close = document.querySelector('.popup-close');
+
+
+
+//tıkla butonuna tıklayınca popup açılır
+buton.addEventListener('click', () => {
+    maninPopup.style.display = 'block';
+})
+
+
+//x tıklandığında popup kapansın
+close.addEventListener('click', () => {
+    maninPopup.style.display = 'none';
+})
+
+
+//herhangi bir yere tıklanınca kapansın
+maninPopup.addEventListener('click', e => {
+    console.log(e.target);
+    if (e.target.className === 'main-popup') {
+        maninPopup.style.display = 'none';
+    }
+
+})
+
+
