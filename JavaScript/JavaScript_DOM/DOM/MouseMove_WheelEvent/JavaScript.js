f83701f47074d24b9de3bd6bb8b96d2f0d77dfc6
Miray Durgun <miraydurgun67@gmail.com>
2023-12-06 21:36:31 +0300

    MouseMove WheelEvent kullanımı

---
+++ JavaScript/JavaScript_DOM/DOM/MouseMove_WheelEvent/JavaScript.js
@@ -0,0 +1,14 @@
+﻿const kutu = document.querySelector('.kutu');
+kutu.addEventListener('mousemove', e => {
+
+    //console.log(e);
+    //console.log(e.offsetX,e.offsetY);
+    kutu.textContent = `x koordinatı ${e.offsetX}, y koordinatı ${e.offsetY}`
+    //kutucuğun üzerinde mousenin olduğu koordinatı yazdırır
+
+})
+
+//wheel mouse tekerleği
+document.addEventListener('wheel', e => {
+    console.log(e.offsetX, e.offsetY);
+})
