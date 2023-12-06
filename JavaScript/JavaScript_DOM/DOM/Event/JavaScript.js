84b99e842bb0f080ee5fffabf97e63a87c06cce0
Miray Durgun <miraydurgun67@gmail.com>
2023-12-06 20:59:25 +0300

    EventListener, Listeye Eleman Ekleme Kaldırma

--- JavaScript/JavaScript_DOM/DOM/Event/JavaScript.js
+++ JavaScript/JavaScript_DOM/DOM/Event/JavaScript.js
@@ -1,32 +1,38 @@
-﻿const secim = document.querySelector('section');
-
-//children 
-/* HTML öğesinin içinde bulunan ve doğrudan alt öğeleri olan öğeleri seçmenizi sağlar. Örneğin, bir <div> öğesinin içinde bulunan <p>, <span>, <ul>, <li> gibi alt öğeleri children özelliği ile erişebilirsiniz.*/
-console.log(secim.children);
-
-//arraye çevirme
-console.log(Array.from(secim.children));
-//from(secim.children) çevirmek istediğimiz kaynağı belirtiriz
-
-
-Array.from(secim.children).forEach(child => {
-    child.classList.add('secim-element');
+﻿const buton = document.querySelector('button');
+//tıklanma işlemini ekliyoruz
+buton.addEventListener('click', () => {
+    console.log('tıklandı');
 })
-/*burada HTML öğesinin doğrudan çocuk öğelerine erişerek her birine secim-element sınıfını ekliyor.
-Array.from() yöntemi, bir dizi olmayan bir nesneyi diziye dönüştürmek için kullanılır.
-Array.from(secim.children) ifadesi, secim adlı bir HTML öğesinin doğrudan çocuk öğelerini bir diziye dönüştürür.
-forEach döngüsü, her bir çocuk öğe için belirli bir işlem yapmak için kullanılır.
-child.classList.add('secim-element'): Bu ifade, child adlı her bir çocuk öğesine secim-element adlı bir CSS sınıfı ekler. Böylelikle, her bir çocuk öğe bu sınıfa sahip olur ve CSS stil dosyasında bu sınıfa özel stil tanımlanabilir.
-*/
 
 
-// parent
-/*JavaScript'te parent terimi, genellikle bir HTML öğesinin doğrudan üst (ebeveyn) öğesine erişimi ifade eder. Bu, DOM (Belge Nesne Modeli) yapısında bir öğenin hiyerarşik yapısını temsil ederken kullanılır.
+//li taglerina ulaşma
 
-Örneğin, bir HTML öğesinin bir üst öğesine (ebeveynine) erişmek istediğinizde, parentElement özelliği kullanılabilir.*/
+const liste = document.querySelectorAll('li');
+//foreach ile li taglerine gezinir
+liste.forEach(eleman => {
+    eleman.addEventListener('click', e => {
+        console.log('Li tagine tıklandı');
+        console.log(e.target);
+        e.target.style.color = 'blue';
+        //tıklayınca rengi değişir
+    })
+})
 
-const baslik = document.querySelector('h2');
-console.log(secim.parentElement);
-//tamamını seçti diğeri gibi tek tek değil
-console.log(secim.parentElement.parentElement);
-//ana elemente bodye gider başka olmadığından
+
+//li lere tıklanınca listeden kaldıracak ekle butonuna tıklayınca li ekleyecek
+
+const ul = document.querySelector('ul');
+liste.forEach(eleman => {
+    eleman.addEventListener('click', e => {
+        e.target.remove();
+        //tıklayınca liyi siler 
+    })
+})
+
+buton.addEventListener('click', () => {
+    const li = document.createElement('li');
+    //li tagi oluşturur
+    li.textContent = 'JavaScript';
+    ul.append(li); //listenin sonuna ekler
+    //  ul.prepend(li); //başına ekler
+})
