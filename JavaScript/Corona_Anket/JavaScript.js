e450605c4fcbf5d6b41d4e539f0b196e2f4e2a2d
Miray Durgun <miraydurgun67@gmail.com>
2023-12-07 16:38:51 +0300

    Korona anket js ile cevaplar kontrol edildi

---
+++ JavaScript/Corona_Anket/JavaScript.js
@@ -0,0 +1,18 @@
+﻿const dogruCevap = ['E', 'E', 'E', 'E'];
+
+const form = document.querySelector('.question-form');
+
+form.addEventListener('submit', e => {
+    e.preventDefault(); // Sayfanın yeniden yüklenmesini engellemek için kullanılır.
+
+    let score = 0;
+    const userAnsers = [form.q1.value, form.q2.value, form.q3.value, form.q4.value];
+
+    userAnsers.forEach((answer, index) => {
+        if (answer === dogruCevap[index]) {
+            score += 25;
+
+        }
+    })
+    console.log(score);
+})
